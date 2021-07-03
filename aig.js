eb11e38ae145a5ce92dcd34f627a75e8e2c660b0
Unfamous Dyson <dougdyson@gmail.com>
2021-07-03 16:12:33 -0700

    refactor test for intro and apology

--- aig.js
+++ aig.js
@@ -1,8 +1,12 @@
+const intros = ['Hello.','Good day.'];
 const apologies = ['We are sorry.','We apologize.'];
 
+const introIndex = Math.floor(Math.random() * (intros.length));
 const apologyIndex = Math.floor(Math.random() * (apologies.length));
 
+const intro = intros[introIndex];
 apology = apologies[apologyIndex];
-console.log(apology);
+console.log(intro, apology);
 
-module.exports = apology;
+exports.intro = intro;
+exports.apology = apology;
--- iag.test.js
+++ iag.test.js
@@ -1,9 +1,8 @@
-const intro     = require('./aig.js');
-const apology   = require('./aig.js');
+const {intro, apology} = require('./aig.js');
 
 const intros    = ['Hello.','Good day.'];
 const apologies = ['We are sorry.', 'We apologize.'];
 
 test('Simple intro', () => expect(intros).toContain(intro));
 
-test('Random apologies', () => expect(apologies).toContain(apology));
+test.skip('Random apologies', () => expect(apologies).toContain(apology));
