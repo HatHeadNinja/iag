7317a6d6059b6dd9691405b498ec19dd84c2bcff
Unfamous Dyson <dougdyson@gmail.com>
2021-07-03 16:33:28 -0700

    fix typo

--- aig.js
+++ aig.js
@@ -1,7 +1,7 @@
 const intros       = ['Hello.','Good day.'];
 const apologies    = ['We are sorry.','We apologize.'];
-const expectations = ['It will take generations to address.','Change will not happen over night.']
-const closings     = ['We look forward to working with our Indigenous partners','Together, we can overcome colonial structures and institutions.']
+const expectations = ['It will take generations to address.','Reconciliation takes time.']
+const closings     = ['We look forward to working with our Indigenous partners.','Together, we can overcome colonial structures and institutions.']
 
 const intro        = intros[Math.floor(Math.random() * (intros.length))];
 const apology      = apologies[Math.floor(Math.random() * (apologies.length))];
--- iag.test.js
+++ iag.test.js
@@ -2,8 +2,8 @@ const {intro, apology, expectation, closing} = require('./aig.js');
 
 const intros       = ['Hello.','Good day.'];
 const apologies    = ['We are sorry.', 'We apologize.'];
-const expectations = ['It will take generations to address.','Change will not happen over night.']
-const closings     = ['We look forward to working with our Indigenous partners','Together, we can overcome colonial structures and institutions.']
+const expectations = ['It will take generations to address.','Reconciliation takes time.']
+const closings     = ['We look forward to working with our Indigenous partners.','Together, we can overcome colonial structures and institutions.']
 
 test('Random intro', () => expect(intros).toContain(intro));
 
