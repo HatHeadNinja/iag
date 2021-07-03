0f42a0a7fd27e3cf9101ce828e788b0d87f7e32f
Unfamous Dyson <dougdyson@gmail.com>
2021-07-03 16:27:18 -0700

    add expectation test

--- aig.js
+++ aig.js
@@ -1,6 +1,6 @@
 const intros     = ['Hello.','Good day.'];
 const apologies  = ['We are sorry.','We apologize.'];
-const closings   = ['It will take generations to address.','Change will not happen over night.']
+const closings = ['We look forward to working with our Indigenous partners','Together, we can overcome colonial structures and institutions.']
 
 const intro = intros[Math.floor(Math.random() * (intros.length))];
 const apology = apologies[Math.floor(Math.random() * (apologies.length))];
