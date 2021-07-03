5d8aca2af8ad95cf6ebb3e018abaa6412c4875ea
Unfamous Dyson <dougdyson@gmail.com>
2021-07-03 16:17:27 -0700

    add random closing test

--- iag.test.js
+++ iag.test.js
@@ -1,8 +1,11 @@
-const {intro, apology} = require('./aig.js');
+const {intro, apology, closing} = require('./aig.js');
 
 const intros    = ['Hello.','Good day.'];
 const apologies = ['We are sorry.', 'We apologize.'];
+const closings   = ['Take generations to address.','Not solved quickly.']
 
-test('Simple intro', () => expect(intros).toContain(intro));
+test('Random intro', () => expect(intros).toContain(intro));
 
 test('Random apologies', () => expect(apologies).toContain(apology));
+
+test('Random closings', () => expect(closings).toContain(closing));
