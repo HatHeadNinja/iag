fe06da62eef12eb164f58114919cb18a564fe7c9
Unfamous Dyson <dougdyson@gmail.com>
2021-07-03 16:27:40 -0700

    update expection test

--- iag.test.js
+++ iag.test.js
@@ -1,11 +1,14 @@
-const {intro, apology, closing} = require('./aig.js');
+const {intro, apology, expectations, closing} = require('./aig.js');
 
-const intros    = ['Hello.','Good day.'];
-const apologies = ['We are sorry.', 'We apologize.'];
-const closings   = ['It will take generations to address.','Change will not happen over night.']
+const intros         = ['Hello.','Good day.'];
+const apologies      = ['We are sorry.', 'We apologize.'];
+const expectations   = ['It will take generations to address.','Change will not happen over night.']
+const closings       = ['We look forward to working with our Indigenous partners','Together, we can overcome colonial structures and institutions.']
 
 test('Random intro', () => expect(intros).toContain(intro));
 
 test('Random apologies', () => expect(apologies).toContain(apology));
+
+test('Random expectations', () => expect(expectations).toContain(expectation));
 
 test('Random closings', () => expect(closings).toContain(closing));
