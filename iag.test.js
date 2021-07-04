fd3d5a11dd7b407d685c082ce8729fc13b1215bb
Unfamous Dyson <dougdyson@gmail.com>
2021-07-03 17:17:19 -0700

    clean up code

--- iag.test.js
+++ iag.test.js
@@ -1,11 +1,11 @@
 const {intro, apology, expectation, closing, statement} = require('./iag.js');
 
 const intros       = ['Hello.','Good day.'];
-const apologies    = ['We are sorry.', 'We apologize.'];
+const apologies    = ['We are sorry.','We apologize.'];
 const expectations = ['It will take generations to address.','Reconciliation takes time.']
 const closings     = ['We look forward to working with our Indigenous partners.','Together, we can overcome colonial structures and institutions.']
 
-test('Random intro selected from intro array', () => expect(intros).toContain(intro));
+test('Random intro selected from intros array', () => expect(intros).toContain(intro));
 
 test('Random apology selected from apologies array', () => expect(apologies).toContain(apology));
 
