b91bc34f0c46ec0163f37f7c3e8a022fee22157f
Unfamous Dyson <dougdyson@gmail.com>
2021-07-03 17:10:17 -0700

    clean code and make tests more clear

--- iag.test.js
+++ iag.test.js
@@ -1,16 +1,16 @@
-const {intro, apology, expectation, closing, statement} = require('./aig.js');
+const {intro, apology, expectation, closing, statement} = require('./iag.js');
 
 const intros       = ['Hello.','Good day.'];
 const apologies    = ['We are sorry.', 'We apologize.'];
 const expectations = ['It will take generations to address.','Reconciliation takes time.']
 const closings     = ['We look forward to working with our Indigenous partners.','Together, we can overcome colonial structures and institutions.']
 
-test('Random intro', () => expect(intros).toContain(intro));
+test('Random intro selected from intro array', () => expect(intros).toContain(intro));
 
-test('Random apologies', () => expect(apologies).toContain(apology));
+test('Random apology selected from apologies array', () => expect(apologies).toContain(apology));
 
-test('Random expectations', () => expect(expectations).toContain(expectation));
+test('Random expectation selected from expectations array', () => expect(expectations).toContain(expectation));
 
-test('Random closings', () => expect(closings).toContain(closing));
+test('Random closing selected from closings array', () => expect(closings).toContain(closing));
 
-test('Statement returned', () => expect(statement).toEqual(expect.anything()));
+test('Statement returned anything', () => expect(statement).toEqual(expect.anything()));
