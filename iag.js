5fe6e0888b58c6fd529cef1c5478668fd3a3d45e
Unfamous Dyson <dougdyson@gmail.com>
2021-07-14 23:49:00 -0700

    refactoring for super simple test

--- iag.js
+++ iag.js
@@ -1,23 +1,27 @@
-const intros       = require('./data/intros.json');
-const apologies    = require('./data/apologies.json');
-const expectations = require('./data/expectations.json');
-const closings     = require('./data/closings.json');
+// const intros       = require('./data/intros.json');
+// const apologies    = require('./data/apologies.json');
+// const expectations = require('./data/expectations.json');
+// const closings     = require('./data/closings.json');
 
-const randomIndex = (a) => Math.floor(Math.random() * (a.length));
+// const randomIndex = (a) => Math.floor(Math.random() * (a.length));
 
-const intro       = intros[randomIndex(intros)];
-const apology     = apologies[randomIndex(apologies)];
-const expectation = expectations[randomIndex(expectations)];
-const closing     = closings[randomIndex(closings)];
-const statement   = intro + ' ' + apology + ' ' + expectation + ' ' + closing;
+// const intro       = intros[randomIndex(intros)];
+// const apology     = apologies[randomIndex(apologies)];
+// const expectation = expectations[randomIndex(expectations)];
+// const closing     = closings[randomIndex(closings)];
+// const statement   = intro + ' ' + apology + ' ' + expectation + ' ' + closing;
 
 
-const newStatement = () => document.getElementById('statement').innerHTML = statement;
+function newStatement () {
+  document.getElementById("statement").innerHTML = "statement";
+}
+
+newStatement();
 //const newStatement = () => alert(intro + ' ' + apology + ' ' + expectation + ' ' + closing);
 
-exports.intro = intro;
-exports.apology = apology;
-exports.expectation = expectation;
-exports.closing = closing;
-exports.statement = statement;
-exports.newStatement = newStatement();
+// exports.intro = intro;
+// exports.apology = apology;
+// exports.expectation = expectation;
+// exports.closing = closing;
+// exports.statement = statement;
+// exports.newStatement = newStatement();
