ba26beefb3c53bfe2cbd11eb95d075048261b948
Unfamous Dyson <dougdyson@gmail.com>
2021-07-16 23:54:53 -0700

    clean up code

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 function readTextFile(file, callback) {
   let rawFile = new XMLHttpRequest();
   rawFile.overrideMimeType("application/json");
-  // rawFile.responseType = 'json';
   rawFile.open("GET", file, true);
   rawFile.onreadystatechange = function() {
       if (rawFile.readyState === 4 && rawFile.status == "200") {
@@ -11,13 +10,10 @@ function readTextFile(file, callback) {
   rawFile.send(null);
 }
 
-function newStatement () {
-  readTextFile('./data.json', function(text){
+function newStatement (file) {
+  readTextFile(file, function(text){
     const data = JSON.parse(text);
-    document.getElementById("statement").innerHTML = data.quote;
+    document.getElementById("statement").innerHTML = data.quote + " - " + data.attribution ;
     console.log(data);
   });
-  //document.getElementById("statement").innerHTML = "statement";
 }
-
-newStatement();
