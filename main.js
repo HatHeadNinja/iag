914f70720dcef4ec88bdff6c6ad3ec15d6e22533
Unfamous Dyson <dougdyson@gmail.com>
2021-07-17 00:04:29 -0700

    remove console log

--- main.js
+++ main.js
@@ -14,6 +14,5 @@ function newStatement (file) {
   readTextFile(file, function(text){
     const data = JSON.parse(text);
     document.getElementById("statement").innerHTML = data.quote + " - " + data.attribution ;
-    console.log(data);
   });
 }
