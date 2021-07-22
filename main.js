9540cac48c98631a7d04b2468d24f8a0008dd7b3
Unfamous Dyson <dougdyson@gmail.com>
2021-07-22 14:38:14 -0700

    clean code

--- main.js
+++ main.js
@@ -1,22 +1,22 @@
 function readTextFile(file, callback){
-  const rawFile = new XMLHttpRequest()
-  rawFile.overrideMimeType("application/json")
-  rawFile.open("GET", file, true)
+  const rawFile = new XMLHttpRequest();
+  rawFile.overrideMimeType("application/json");
+  rawFile.open("GET", file, true);
   rawFile.onreadystatechange = function(){
       if (rawFile.readyState === 4 && rawFile.status == "200"){
-          callback(rawFile.responseText)
+          callback(rawFile.responseText);
       }
   }
   rawFile.send(null);
 }
 
 function newStatement(){
-  document.getElementById("statement").innerHTML = "Generating statement..."
+  document.getElementById("statement").innerHTML = "Generating statement...";
   readTextFile('./data.json', function(text){
     const randomIndex = (array) => Math.floor(Math.random() * (array.length));
-    const statements = JSON.parse(text)
-    const opening = statements.openings[randomIndex(statements.openings)].quote
-    const closing = statements.closings[randomIndex(statements.closings)].quote
+    const statements = JSON.parse(text);
+    const opening = statements.openings[randomIndex(statements.openings)].quote;
+    const closing = statements.closings[randomIndex(statements.closings)].quote;
     document.getElementById("statement").innerHTML = opening + " " + closing;
-  });
+  })
 }
