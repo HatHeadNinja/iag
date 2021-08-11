bc85df15e7b1b96ddc8361ed62bf00a2c8c702ce
Unfamous Dyson <dougdyson@gmail.com>
2021-08-10 23:58:19 -0700

    psudo-code delay

--- src/main.js
+++ src/main.js
@@ -12,12 +12,13 @@ function readTextFile(file, callback){
 
 function newStatement(){
   document.getElementById("statement").innerHTML = "Generating...";
+  // need to figure out where to put the animation delay.
   readTextFile('./data/data.json', function(text){
-    // need to figure out where to put the animation delay.
     const randomIndex = (array) => Math.floor(Math.random() * (array.length));
     const statements = JSON.parse(text);
     const opening = statements.openings[randomIndex(statements.openings)].quote;
     const closing = statements.closings[randomIndex(statements.closings)].quote;
     document.getElementById("statement").innerHTML = opening + " " + closing;
   })
+  // end delay
 }
