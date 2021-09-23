86e843b562bffcfc1db1ceedb63f31bf164d4e25
Unfamous Dyson <dougdyson@gmail.com>
2021-09-22 22:32:17 -0700

    lint code

--- src/main.js
+++ src/main.js
@@ -24,4 +24,4 @@ function newStatement(){
     // delay timer for animation and display concatenated statement
     setTimeout(() => {document.getElementById("statement").innerHTML = opening + ' ' + closing;}, 500);
   });
-}
+};
