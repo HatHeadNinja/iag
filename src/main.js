50035a424c36834c7b196bbc84fff9cc3965fdd7
Unfamous Dyson <dougdyson@gmail.com>
2021-10-11 16:25:59 -0700

    concatenate 2 unique quotes

--- src/main.js
+++ src/main.js
@@ -18,17 +18,21 @@ function newStatement(){
   wordsBtn.disabled   = true;
   statement.innerHTML = 'Generating...';
 
-  // TODO: make sure the two random quotes are not the same quote
-  
   readJSONFile('./data/data.json', (text) => {
     const statements  = JSON.parse(text);
+
+    // select 2 random quotes and make sure they are not the same
     const randomIndex = (array) => Math.floor(Math.random() * (array.length));
+    const opening = statements[randomIndex(statements)].quote;
+    const closing = (opening, statements) => {
+      const endQuote = statements[randomIndex(statements)].quote;
+      return (opening === endQuote) ? closing(opening, statements) : endQuote;
+    }
+    const randomStatement = opening + " " + closing(opening, statements);
     
     // display concatenated statement, re-enable button, set focus to action button
     setTimeout(() => {
-      statement.innerHTML = statements[randomIndex(statements)].quote
-                            + ' ' 
-                            + statements[randomIndex(statements)].quote;
+      statement.innerHTML = randomStatement;
       wordsBtn.disabled   = false;
       document.getElementById("action").focus();
       }, 
