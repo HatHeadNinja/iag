ebca7a199d98e11f067cebf4cac196c73d63bacc
Unfamous Dyson <dougdyson@gmail.com>
2021-10-12 16:21:24 -0700

    make delay calculated by statement length

--- src/main.js
+++ src/main.js
@@ -22,13 +22,16 @@ function newStatement(){
     const statements = JSON.parse(text);
 
     // select 2 random quotes and make sure they are not the same
-    // assumes at least 2 quotes available, or else infinite loop
-    // however user can always click the Action button (conversion)
     const randomIndex     = () => Math.floor(Math.random() * (statements.length));
     const openingQuote    = statements[randomIndex()].quote;
     const getClosingQuote = () => {
-      const closingQuote  = statements[randomIndex()].quote;
-      return (closingQuote != openingQuote) ? closingQuote : getClosingQuote();
+      // check there are at least 2 quotes to select from
+      if (statements.length < 2) {
+        return null;
+      } else {
+        const closingQuote  = statements[randomIndex()].quote;
+        return (closingQuote != openingQuote) ? closingQuote : getClosingQuote();
+      }
     }
     
     // display concatenated statement, re-enable words button, set focus to action button
@@ -37,7 +40,7 @@ function newStatement(){
         wordsBtn.disabled   = false;
         document.getElementById("action").focus();
       }, 
-      500 // millisecond delay for animation
+      statement.innerHTML.length * 65 // millisecond delay for animation
     ); 
   });
 };
