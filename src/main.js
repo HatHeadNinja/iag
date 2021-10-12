a50df419936db5f5cd9c3db8cf8d45178b154fdd
Unfamous Dyson <dougdyson@gmail.com>
2021-10-11 17:45:33 -0700

    clean code

--- src/main.js
+++ src/main.js
@@ -26,7 +26,7 @@ function newStatement(){
     const openingQuote    = statements[randomIndex()].quote;
     const getClosingQuote = (openingQuote, statements) => {
       const closingQuote  = statements[randomIndex()].quote;
-      return (openingQuote != closingQuote) ? closingQuote : getClosingQuote(openingQuote, statements);
+      return (closingQuote != openingQuote) ? closingQuote : getClosingQuote(openingQuote, statements);
     }
     const randomStatement = openingQuote + " " + getClosingQuote(openingQuote, statements);
     
