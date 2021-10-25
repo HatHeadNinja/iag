f04b943b026ce33abe637bea5ff56d470aa8dfb0
Unfamous Dyson <dougdyson@gmail.com>
2021-10-24 23:55:31 -0700

    clean up code

--- src/main.js
+++ src/main.js
@@ -13,38 +13,33 @@ function readJSONFile(file, callback){
 }
 
 function loadAllQuotes(){
-
   readJSONFile('./data/data.json', (text) => {
     allQuotes = JSON.parse(text);
   })
 }
 
-
 function newStatement(){
-  
   // disable Words button and display intialization message
   const wordsBtn      = document.getElementById("words");
   const statement     = document.getElementById("statement");
   wordsBtn.disabled   = true;
   statement.innerHTML = 'Generating...';
 
-  readJSONFile('./data/data.json', (text) => {
-    
-    // select 2 random quotes and make sure they are not the same
-    // const statements      = JSON.parse(text);
-    const randomIndex     = () => Math.floor(Math.random() * (allQuotes.length));
-    const openingQuote    = allQuotes[randomIndex()].quote;
-    const getClosingQuote = () => {
-      const closingQuote  = allQuotes[randomIndex()].quote;
-      return (closingQuote != openingQuote) ? closingQuote : getClosingQuote();
-    }
-    
-    // display concatenated statement, re-enable words button
-    setTimeout(() => {
-        statement.innerHTML = openingQuote + " " + getClosingQuote();
-        wordsBtn.disabled   = false;
-      },
-      statement.innerHTML.length * 50 // millisecond delay for animation
-    )
-  })
+  // select 2 random quotes and make sure they are not the same
+  // const statements      = JSON.parse(text);
+  const randomIndex     = () => Math.floor(Math.random() * (allQuotes.length));
+  const openingQuote    = allQuotes[randomIndex()].quote;
+  const getClosingQuote = () => {
+    const closingQuote  = allQuotes[randomIndex()].quote;
+    return (closingQuote != openingQuote) ? closingQuote : getClosingQuote();
+  }
+  
+  // display concatenated statement, re-enable words button
+  setTimeout(() => {
+      statement.innerHTML = openingQuote + " " + getClosingQuote();
+      wordsBtn.disabled   = false;
+    },
+    statement.innerHTML.length * 50 // millisecond delay for animation
+  )
+  
 }
